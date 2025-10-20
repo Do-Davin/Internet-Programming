de895057be4a06ab3e3b3b9e1488cdf895a6cd66
Do Davin <dodavin96@gmail.com>
2025-10-21 00:28:09 +0700

    Add new version

--- Practice01/script.js
+++ Practice01/script.js
@@ -5,7 +5,7 @@ const width = 80;
 const height = 40;
 const rows = 12;
 const cols = 15;
-const mortar = 4; 
+const mortar = 5; 
 
 for (let i = 0; i < rows; i++) {
   const offset = (i % 2 === 0) ? 0 : width / 2;
@@ -14,13 +14,7 @@ for (let i = 0; i < rows; i++) {
     const posX = k * (width + mortar) - offset;
     const posY = i * (height + mortar);
 
-    if (posX + width > 0 && posX < canvas.width) {
-      ctx.fillStyle = "#b55239"; // Brick color 
-      ctx.fillRect(posX, posY, width, height);
-
-      ctx.strokeStyle = "black"; 
-      ctx.lineWidth = 4;
-      ctx.strokeRect(posX, posY, width, height);
-    }
+    ctx.fillStyle = "#b55239"; // Brick color 
+    ctx.fillRect(posX, posY, width, height);
   }
 }
