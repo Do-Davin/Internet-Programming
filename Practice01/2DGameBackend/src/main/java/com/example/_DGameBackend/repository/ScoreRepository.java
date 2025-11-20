37bd574baa1a5f5785c91f0dc918cd524689435a
Do Davin <dodavin96@gmail.com>
2025-11-20 09:36:13 +0700

    Remove unrelated folders from branch

--- Practice01/2DGameBackend/src/main/java/com/example/_DGameBackend/repository/ScoreRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example._DGameBackend.repository;
-
-import com.example._DGameBackend.entity.Score;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ScoreRepository extends JpaRepository<Score, Long> {}
