87fe68deb5061520fcc67b8c54b1e831b5ae23e3
Do Davin <dodavin96@gmail.com>
2025-12-12 14:43:13 +0700

    Completed Lab

--- Practice02/nest-micro-lab/api-gateway/src/app.module.ts
+++ Practice02/nest-micro-lab/api-gateway/src/app.module.ts
@@ -1,7 +1,24 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { OrdersModule } from './orders/orders.module';
+import { ReceiptsModule } from './receipts/receipts.module';
 
 @Module({
-  imports: [OrdersModule],
+  imports: [
+    OrdersModule,
+    ReceiptsModule,
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'postgres',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'order-worker',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
 })
 export class AppModule {}
